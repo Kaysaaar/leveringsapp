fddae4f94661fa23cb0c6afd9d1ca99a0dd50b7a
Kaysaaar <michael.jensen91@hotmail.com>
2026-03-24 20:44:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'leveringsapp-v13';
+const CACHE_NAME = 'leveringsapp-v14';
 const APP_SHELL = [
   './',
   './index.html',
@@ -57,4 +57,10 @@ self.addEventListener('fetch', event => {
       });
     })
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
